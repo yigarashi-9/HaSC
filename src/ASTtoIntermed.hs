07217cb167509704515acca4ef6fc86390192cf5
yu-i9 <allgemeiner.pc@gmail.com>
2015-07-16 13:14:22 +0900

    中間表現の生成に成功した

--- src/ASTtoIntermed.hs
+++ src/ASTtoIntermed.hs
@@ -1,49 +1,73 @@
 module ASTtoIntermed where
 
 import Control.Monad
-import Control.Monad.State.Strict
+import Control.Monad.State
+import qualified Control.Monad.Trans.State as S
 import Data.List
 
 import Semantic
+import ObjInfo
 import AnalyzedAST
 import IntermedSyntax
 
+
+{- *****************************
+          Utils for Env
+   ***************************** -}
+
 type VarNum    = Int
+type LabelNum  = Int
 type Reuseable = [VarNum]
-type VarEnv    = State (Reuseable, VarNum)
+type IREnv     = S.StateT (Reuseable, VarNum) (State LabelNum)
 
-runEnv :: VarEnv a -> a
-runEnv v = evalState v ([], 0)
+runEnv :: IREnv a -> a
+runEnv v = evalState (S.evalStateT v ([], 0)) 0
 
-freshVar :: VarEnv IVar
+freshVar :: IREnv IVar
 freshVar = do
-  (reuse, num) <- get
+  (reuse, num) <- S.get
   newnum <- case reuse of
-              []     -> do put ([], num+1)
+              []     -> do S.put ([], num+1)
                            return num
-              (n:ns) -> do put (ns, num)
+              (n:ns) -> do S.put (ns, num)
                            return n
   return $ makeVar newnum
-    where makeVar n = Info (("@" ++) . show $ n, ObjInfo Var CTemp (-1))
+    where makeVar n = Info (ObjInfo (("@" ++) . show $ n) Var CTemp (-1))
 
-freshVars :: Int -> VarEnv [IVar]
+freshVars :: Int -> IREnv [IVar]
 freshVars n = replicateM n freshVar
 
-resetVars :: VarEnv ()
-resetVars = put ([], 0)
+resetVars :: IREnv ()
+resetVars = S.put ([], 0)
 
-collectUnuseVar :: IDecl -> VarEnv ()
-collectUnuseVar (IVarDecl (Info (('@':varNum), _)))
-    = do (reuse, num) <- get
-         when (not $ read varNum `elem` reuse) (put (sort $ (read varNum):reuse, num))
+freshLabel :: IREnv Label
+freshLabel = do n <- lift get
+                lift (put (n+1))
+                return $ "@label" ++ show n
+
+freshLabels :: Int -> IREnv [Label]
+freshLabels n = replicateM n freshLabel
+
+
+collectUnuseVar :: IVar -> IREnv ()
+collectUnuseVar (Info (ObjInfo ('@':varNum) _ _ _))
+    = do (reuse, num) <- S.get
+         when (not $ (read varNum) `elem` reuse) (put (sort $ (read varNum):reuse, num))
 collectUnuseVar _ = return ()
 
-collectUnuseVars :: [IDecl] -> VarEnv ()
+
+collectUnuseVars :: [IVar] -> IREnv ()
 collectUnuseVars = mapM_ collectUnuseVar
 
 result :: [IVar] -> IVar
 result [] = error "unexpected vars, sorry."
 result l  = head l
+
+
+
+{- *******************************
+            Convert AST
+   ******************************* -}
 
 astToIntermed :: A_Program -> IProgram
 astToIntermed prog = runEnv $ mapM convDecl prog
@@ -51,56 +75,68 @@ astToIntermed prog = runEnv $ mapM convDecl prog
 showIProgram :: IProgram -> String
 showIProgram prog = concat $ intersperse "\n" (map show prog)
 
-convDecl :: A_EDecl -> VarEnv IDecl
+convDecl :: A_EDecl -> IREnv IDecl
 convDecl (A_Decl var)              = return (IVarDecl $ Info var)
 convDecl (A_Func _ var parms body)
-    = do (_, stmts) <- resetVars >> convStmt body
-         let [cmpdStmt] = stmts
-         return $ IFunDecl (Info var) (map Info parms) cmpdStmt
+    = do resetVars
+         (_, stmts) <- convStmt body
+         return $ IFunDecl (Info var) (map Info parms) stmts
 
 
-convStmt :: A_Stmt -> VarEnv ([IVar], [IStmt])
-convStmt (A_EmptyStmt)  = return ([], [IEmpty])
+convStmt :: A_Stmt -> IREnv ([IVar], [ICode])
+convStmt (A_EmptyStmt)  = return ([], [])
 convStmt (A_ExprStmt e) = convExpr e
-convStmt (A_DeclStmt _) = error "This error should never happen."
 convStmt (A_IfStmt _ cond tr fls)
-    = do (varsCond, makeCond)   <- convExpr cond
-         (varsTrue, stmtTrue)   <- convStmt tr
+    = do (varsCond,  stmtCond)  <- convExpr cond
+         (varsTrue,  stmtTrue)  <- convStmt tr
          (varsFalse, stmtFalse) <- convStmt fls
+         (lfls:lexit:[])    <- freshLabels 2
          return (varsCond ++ varsTrue ++ varsFalse,
-                 makeCond ++ [IIf (result varsCond) stmtTrue stmtFalse])
+                 stmtCond
+                 ++ [IJumpFls (result varsCond) lfls]
+                 ++ stmtTrue
+                 ++ [IJump lexit,
+                     ILabel lfls]
+                 ++ stmtFalse
+                 ++ [ILabel lexit])
 convStmt (A_WhileStmt _ cond body)
-    = do (varsCond, makeCond)   <- convExpr cond
-         (varsBody, stmtBody)   <- convStmt body
+    = do (varsCond, stmtCond) <- convExpr cond
+         (varsBody, stmtBody) <- convStmt body
+         (lloop:lexit:[])     <- freshLabels 2
          return (varsCond ++ varsBody,
-                 makeCond ++ [IWhile (result varsCond) (stmtBody ++ makeCond)])
+                 [ILabel lloop]
+                 ++ stmtCond
+                 ++ [IJumpFls (result varsCond) lexit]
+                 ++ stmtBody
+                 ++ stmtCond
+                 ++ [IJump lloop, ILabel lexit])
 convStmt (A_ReturnStmt _ e)
     = do (vars, stmts) <- convExpr e
          return (vars, stmts ++ [IReturn (result vars)])
-convStmt (A_RetVoidStmt _) = return ([], [IEmpty])
+convStmt (A_RetVoidStmt _) = return ([], [])
 convStmt (A_CompoundStmt stmts)
     = do (decls, stmts) <- foldM foldCmpdStmt ([], []) stmts
          collectUnuseVars decls
-         return ([], [ICompound (map head . group . sort $ decls) stmts])
+         return (decls, stmts)
 
-foldCmpdStmt :: ([IDecl], [IStmt]) -> A_Stmt -> VarEnv ([IDecl], [IStmt])
+foldCmpdStmt :: ([IVar], [ICode]) -> A_Stmt -> IREnv ([IVar], [ICode])
 foldCmpdStmt (idecl, istmt) stmt
     = case stmt of
-        (A_DeclStmt l) -> return (idecl ++ map (IVarDecl . Info) l, istmt)
+        (A_DeclStmt l) -> return (idecl ++ map Info l, istmt)
         otherStmt      -> do (vars, stmts) <- convStmt otherStmt
-                             return (idecl ++ map IVarDecl (extractTemp vars),
+                             return (idecl ++ (extractTemp vars),
                                      istmt ++ stmts)
 
 {- 残念ながらプログラム上で宣言された変数も convExpr の結果に含まれてしまう．
    CompoundStmt の decl に入ってしまうと困るので削除する．-}
 extractTemp :: [IVar] -> [IVar]
-extractTemp = filter ((== '@') . head . fst . info)
+extractTemp = filter ((== '@') . head . objName . info)
 
 
 {- 与えられた Expr の最終的な結果が格納される変数を
    常に結果タプル第1要素の先頭に配置する．
    それによってあとの変換で変数を使用することができる．-}
-convExpr :: A_Expr -> VarEnv ([IVar], [IStmt])
+convExpr :: A_Expr -> IREnv ([IVar], [ICode])
 convExpr (A_AssignExpr _ dest src)
     = case dest of
         (A_UnaryPrim _ "*" dst) -> do (vars1, stmts1) <- convExpr dst
@@ -110,7 +146,7 @@ convExpr (A_AssignExpr _ dest src)
                                              ++ [IWrite (result vars1)  (result vars2)])
         (A_IdentExpr vdst) -> do (vars, stmts) <- convExpr src
                                  return (vars, stmts ++
-                                         [ILet (Info vdst) (IVarExp $ result vars)])
+                                         [ILet (Info vdst) (result vars)])
 convExpr (A_UnaryPrim _ "*" e)
     = do dest <- freshVar
          (vars, stmts) <- convExpr e
@@ -118,7 +154,7 @@ convExpr (A_UnaryPrim _ "*" e)
 convExpr (A_UnaryPrim _ "&" e)
     = do dest <- freshVar
          (vars, stmts) <- convExpr e
-         return (dest:vars, stmts ++ [ILet dest (IAddr $ result vars)])
+         return (dest:vars, stmts ++ [IAddr dest (result vars)])
 convExpr (A_BinaryPrim _ op e1 e2)
     | op `elem` ["+", "-", "*", "/"]
         = if isPointer e1
@@ -128,66 +164,81 @@ convExpr (A_BinaryPrim _ op e1 e2)
         = do (vars1, stmts1) <- convExpr e1
              (vars2, stmts2) <- convExpr e2
              dest            <- freshVar
-             return (dest:(vars1 ++ vars2), stmts1 ++ stmts2 ++
-                             [ILet dest $ IRelop op (result vars1) (result vars2)])
+             return (dest:(vars1 ++ vars2),
+                     stmts1
+                     ++ stmts2
+                     ++ [IRelop op dest (result vars1) (result vars2)])
     | op == "&&"
-        = do (vars1, stmts1) <- convExpr e1
-             (vars2, stmts2) <- convExpr e2
-             dest            <- freshVar
-             return (dest:(vars1 ++ vars2), stmts1 ++ stmts2 ++
-                     [IIf (result vars1) [IIf (result vars2) [ILet dest (IInt 1)]
-                                                             [ILet dest (IInt 0)]]
-                                         [ILet dest (IInt 0)]])
+        = do (vars1, stmts1)  <- convExpr e1
+             (vars2, stmts2)  <- convExpr e2
+             dest             <- freshVar
+             (lfls:lexit:[])  <- freshLabels 2
+             return (dest:(vars1 ++ vars2),
+                     stmts1
+                     ++ stmts2
+                     ++ [IJumpFls (result vars1) lfls,
+                         IJumpFls (result vars2) lfls,
+                         ILet dest (Const 1),
+                         IJump lexit,
+                         ILabel lfls,
+                         ILet dest (Const 0),
+                         ILabel lexit])
     | op == "||"
         = do (vars1, stmts1) <- convExpr e1
              (vars2, stmts2) <- convExpr e2
              dest            <- freshVar
-             return (dest:(vars1 ++ vars2), stmts1 ++ stmts2 ++
-                     [IIf (result vars1) [ILet dest (IInt 1)]
-                                         [IIf (result vars2) [ILet dest (IInt 1)]
-                                                             [ILet dest (IInt 0)]]])
+             (ltr:lexit:[])  <- freshLabels 2
+             return (dest:(vars1 ++ vars2),
+                     stmts1
+                     ++ stmts2
+                     ++ [IJumpTr (result vars1) ltr,
+                         IJumpTr (result vars1) ltr,
+                         ILet dest (Const 0),
+                         IJump lexit,
+                         ILabel ltr,
+                         ILet dest (Const 1),
+                         ILabel lexit])
 convExpr (A_ApplyFunc _ func args)
     = do res <- mapM convExpr args
-         if fst func == "print"
+         if objName func == "print"
          then let [(vars, stmts)] = res
               in return (vars, stmts ++ [IPrint (result vars)])
-         else do  dest <- freshVar
-                  let resArgs = map (head . fst) res
-                      resVars = concat $ map fst res
-                      resStmt = concat $ map snd res
-                  return (dest:resVars, resStmt ++ [ICall dest (Info func) resArgs])
+         else do dest <- freshVar
+                 let resArgs = map (head . fst) res
+                     resVars = concat $ map fst res
+                     resStmt = concat $ map snd res
+                 return (dest:resVars, resStmt ++ [ICall dest (Info func) resArgs])
 convExpr (A_MultiExpr es)
     = do res <- mapM convExpr es
          -- 最後の結果が先頭にならないといけないので reverse
          return (concat . reverse $ map fst res, concat $ map snd res)
 convExpr (A_Constant n)
-    = freshVar >>= (\dest -> return ([dest], [ILet dest (IInt n)]))
+    = freshVar >>= (\dest -> return ([dest], [ILet dest (Const n)]))
 convExpr (A_IdentExpr i) = if isArray i
                            then do v <- freshVar
-                                   return $ (v:[Info i], [ILet v (IAddr (Info i))])
+                                   return $ (v:[Info i], [IAddr v (Info i)])
                            else return ([Info i], [])
 
-isArray :: A_Idnentifier -> Bool
-isArray (_, (ObjInfo _ ty _))
-    = case ty of
-        (CArray _ _) -> True
-        _            -> False
+isArray :: ObjInfo -> Bool
+isArray info = case objCtype info of
+                 (CArray _ _) -> True
+                 _            -> False
 
-pointerCalc :: String -> A_Expr -> A_Expr -> VarEnv ([IVar], [IStmt])
+pointerCalc :: String -> A_Expr -> A_Expr -> IREnv ([IVar], [ICode])
 pointerCalc op e1 e2
     = do (vars1, stmts1) <- convExpr e1
          (vars2, stmts2) <- convExpr e2
          (dest:v1:v2:[]) <- freshVars 3
          return (dest:v1:v2:(vars1 ++ vars2), stmts1 ++ stmts2 ++
-                 [ILet v1 (IInt 4),
-                  ILet v2 (IAop "*" v1 (result vars2)),
-                  ILet dest $ IAop op (result vars1) v2])
+                 [ILet v1 (Const 4),
+                  IAop "*" v2   v1             (result vars2),
+                  IAop op  dest (result vars1) v2])
 
 
-arithCalc :: String -> A_Expr -> A_Expr -> VarEnv ([IVar], [IStmt])
+arithCalc :: String -> A_Expr -> A_Expr -> IREnv ([IVar], [ICode])
 arithCalc op e1 e2
     = do (vars1, stmts1) <- convExpr e1
          (vars2, stmts2) <- convExpr e2
          dest            <- freshVar
          return (dest:(vars1 ++ vars2), stmts1 ++ stmts2 ++
-                 [ILet dest $ IAop op (result vars1) (result vars2)])
+                 [IAop op dest (result vars1) (result vars2)])
--- src/AnalyzedAST.hs
+++ src/AnalyzedAST.hs
@@ -1,84 +1,25 @@
 module AnalyzedAST where
 
 import AST
+import ObjInfo
 import Text.Parsec.Pos
-import Data.List
-import Control.Monad.State.Strict
 
 {- オブジェクトの情報を収集するためのデータ型 -}
-
 type Check = Either String
-
-data ObjInfo  = ObjInfo { kind  :: Kind,
-                          ctype :: CType,
-                          level :: Level
-                        }deriving(Eq, Ord)
-
-instance Show ObjInfo where
-    show (ObjInfo _ _ lev) = show lev
-
-type Level = Int
-data Kind  = Var | Func | FuncProto | Parm deriving(Show, Eq, Ord)
-data CType = CVoid
-           | CInt
-           | CPointer CType
-           | CArray   CType Integer
-           | CFun     CType [CType]
-           | CTemp
-           deriving(Ord)
-
-instance Show CType where
-    show (CInt)           = "int"
-    show (CTemp)          = "temp"
-    show (CVoid)          = "void"
-    show (CPointer CInt)  = "int *"
-    show (CArray ty size) = show ty ++ "[" ++ show size ++ "]"
-    show (CFun ty args)   = concat ["(", concat $ intersperse ", " (map show args),
-                                    ") -> ", show ty]
-    show _                = "*** complex type... cannot show ***"
-
-instance Eq CType where
-    (==) CInt             CInt             = True
-    (==) CTemp            CTemp            = True
-    (==) CVoid            CVoid            = True
-    (==) (CPointer ty1)   (CPointer ty2)   = ty1 == ty2
-    (==) (CArray ty1 _)   (CArray ty2 _)   = ty1 == ty2
-    (==) (CFun ty1 args1) (CFun ty2 args2) = (ty1 == ty2) && (args1 == args2)
-    (==) (CArray ty1 _)   (CPointer ty2)   = ty1 == ty2
-    (==) (CPointer ty1)   (CArray ty2 _)   = ty1 == ty2
-    (==) _ _ = False
-
-
-convType :: DeclType -> CType
-convType (DeclPointer ty) = CPointer (convType ty)
-convType (DeclInt)        = CInt
-convType (DeclVoid)       = CVoid
-
-containVoid :: CType -> Bool
-containVoid (CVoid)       = True
-containVoid (CArray ty _) = containVoid ty
-containVoid (CPointer ty) = containVoid ty
-containVoid _             = False
-
--- void かそうでないかが重要なので、max を使って型を合成する
-synType :: CType -> CType -> CType
-synType = max
 
 
 {- 収集したオブジェクト情報を埋め込むための新たな木。
    A_ は Analyzed の頭文字。
    SourcePos は必要になるところだけに埋め込んでいる。 -}
-
 type A_Program = [A_EDecl]
-type A_Idnentifier = (Identifier, ObjInfo)
 
-data A_EDecl = A_Decl A_Idnentifier
-             | A_Func SourcePos A_Idnentifier [A_Idnentifier] A_Stmt
+data A_EDecl = A_Decl ObjInfo
+             | A_Func SourcePos ObjInfo [ObjInfo] A_Stmt
                deriving(Eq, Show)
 
 data A_Stmt = A_EmptyStmt
             | A_ExprStmt     A_Expr
-            | A_DeclStmt     [A_Idnentifier]
+            | A_DeclStmt     [ObjInfo]
             | A_CompoundStmt [A_Stmt]
             | A_IfStmt       SourcePos A_Expr A_Stmt A_Stmt
             | A_WhileStmt    SourcePos A_Expr A_Stmt
@@ -89,8 +30,8 @@ data A_Stmt = A_EmptyStmt
 data A_Expr = A_AssignExpr SourcePos A_Expr A_Expr
             | A_UnaryPrim  SourcePos String A_Expr
             | A_BinaryPrim SourcePos String A_Expr A_Expr
-            | A_ApplyFunc  SourcePos A_Idnentifier [A_Expr]
+            | A_ApplyFunc  SourcePos ObjInfo [A_Expr]
             | A_MultiExpr  [A_Expr]
             | A_Constant   Integer
-            | A_IdentExpr  A_Idnentifier
+            | A_IdentExpr  ObjInfo
               deriving(Eq, Show)
